ccb9cdd478f9320ad4393c3c7e3b586b9ceca748
sbluff <alonsofmira@gmail.com>
2023-09-11 14:38:02 +0200

    Generate graphs comparing configurations

--- ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/generate_graphs_10m.py
+++ ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/generate_graphs_10m.py
@@ -14,13 +14,30 @@ sampled_distance = 20
 
 simulation_type = [ f.path for f in os.scandir('./') if f.is_dir() ]
 for simulation in simulation_type:
+    df = pd.read_csv('./meassurements.csv')
+    df = df.loc[(df.meassurement_type == simulation.replace('./', '')) & (df.real_distance == 20)]
+    print(df.size)
+    df['error'].hist(range=[0,5], edgecolor='black', grid=True, legend=True, by=df['ftm_per_burst'], bins=30)
+    plt.xlabel('Error')
+    plt.ylabel("Frequency");
+    plt.savefig('./' + simulation +'/ftm_per_burst-histograms-' + str(20) + "m.pdf")
+    plt.clf()
+    
+    df['error'].hist(range=[0,5], edgecolor='black', grid=True, legend=True, bins=30, by=df['burst_exponent'])
+    plt.xlabel('Error(m)')
+    plt.ylabel("Frequency(#)");
+    plt.savefig('./' + simulation +'/burst_exponent-histograms-' + str(20) + "m.pdf")
+    
+    plt.close()
+    
+    
     parameters_configuration = [ f.path for f in os.scandir (simulation + '/') if f.is_dir() ]
     for parameter_config in parameters_configuration:
         parameters = parameter_config.replace(simulation + '/', '')
         parameters = parameters.split('-')
         id = str(parameters[0]) +  '-' + str(parameters[1]) + '-' + str(parameters[2]) + '-' + str(parameters[3]) + '-' + str(parameters[4])
         path = simulation + '/' + id
-        print(path)
+        print(path)        
         df = pd.read_csv('./meassurements.csv')
         df = df.loc[(df.meassurement_type == simulation.replace('./', '')) & (df.min_delta_ftm == int(parameters[0])) & (df.burst_period == int(parameters[1])) & (df.burst_exponent == int(parameters[2])) & (df.burst_duration == int(parameters[3])) & (df.ftm_per_burst == int(parameters[4]))]
         if len(df.index) != 0:
@@ -30,18 +47,21 @@ for simulation in simulation_type:
             plt.clf()
             
             #get the sampled distance data !!
-            df = df.loc[df.real_distance == sampled_distance]
-            
+            df = df.loc[df.real_distance == 20]
+        
             #histogram
             histogram = df['error']         
-            histogram.plot(kind="hist", alpha=0.7, edgecolor='black', color='purple', bins= 30, title=simulation + ' ' + id)
-            plt.title("Histogram (" + str(sampled_distance) + "m) "+  simulation  + ' ' + id)
-            plt.savefig('./' + path + '/' + "histogram.pdf")
+            histogram.plot(kind="hist", xlim=(0,5), alpha=0.7, edgecolor='black', color='purple', bins= 25, title=simulation + ' ' + id, grid=True)
+            plt.title("Histogram (" + str(20) + "m) "+  simulation  + ' ' + id)
+            
+
+            plt.savefig('./' + path + '/histogram-' + str(20) + "m.pdf")
             plt.clf()
             
             #density
-            df.error.plot.density(color='green')
-            plt.title("Error density (" + str(sampled_distance) + "m) "+  simulation  + ' ' + id)
+            df.error.plot.density(color='green', xlim=(0,5), grid=True)
+            plt.title("Error density (" + str(20) + "m) "+  simulation  + ' ' + id)
             plt.gca().xaxis.set_major_formatter(FormatStrFormatter('%d m'))
-            plt.savefig('./' + path + '/' + "density.pdf")
+            
+            plt.savefig('./' + path + '/density-' + str(20) + "m.pdf")
             plt.close()
