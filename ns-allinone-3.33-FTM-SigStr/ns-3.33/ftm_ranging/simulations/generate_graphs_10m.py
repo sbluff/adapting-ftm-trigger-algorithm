18016c35c5a79b8056736bf2c57970057b027845
sbluff <alonsofmira@gmail.com>
2023-09-29 19:41:15 +0200

    save graphs in folders

--- ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/generate_graphs_10m.py
+++ ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/generate_graphs_10m.py
@@ -19,19 +19,23 @@ matplotlib.style.use('ggplot')
 def createComparisonGraphs(simulation_type, df):
     hist = df
     ftm_per_burst_values = df['ftm_per_burst'].unique()
+    path = './' + simulation_type
+    if not os.path.isdir(path):
+        os.makedirs(path)
+        
     for value in ftm_per_burst_values:
         hist = df.loc[(df.ftm_per_burst == value)]
         hist['error'].hist(range=[0,4], edgecolor='black', grid=True, label=value, bins=20,alpha=0.5)      
     plt.xlabel('Error')
     plt.ylabel("Frequency")
     plt.legend()
-    plt.savefig('./' + simulation_type +'/ftm_per_burst-overlap-histograms-' + str(sampled_distance) + "m.pdf")
+    plt.savefig(path + '/ftm_per_burst-overlap-histograms-' + str(sampled_distance) + "m.pdf")
     plt.clf()
     
     df['error'].hist(range=[0,4], edgecolor='black', grid=True, legend=True, bins=30, by=df['ftm_per_burst'])
     plt.xlabel('Error(m)')
     plt.ylabel("Frequency(#)");
-    plt.savefig('./' + simulation_type +'/ftm_per_burst-histograms-' + str(sampled_distance) + "m.pdf")
+    plt.savefig(path + '/ftm_per_burst-histograms-' + str(sampled_distance) + "m.pdf")
     
     plt.clf()
     
@@ -40,7 +44,7 @@ def createComparisonGraphs(simulation_type, df):
     df['error'].hist(range=[0,4], edgecolor='black', grid=True, legend=True, bins=30, by=df['burst_exponent'])
     plt.xlabel('Error(m)')
     plt.ylabel("Frequency(#)");
-    plt.savefig('./' + simulation_type +'/burst_exponent-histograms-' + str(sampled_distance) + "m.pdf")
+    plt.savefig(path + '/burst_exponent-histograms-' + str(sampled_distance) + "m.pdf")
     
     plt.close()
     
@@ -52,7 +56,7 @@ def createComparisonGraphs(simulation_type, df):
     plt.xlabel('Error')
     plt.ylabel("Frequency")
     plt.legend()
-    plt.savefig('./' + simulation_type +'/burst_exponent-overlap-histograms-' + str(sampled_distance) + "m.pdf")
+    plt.savefig(path + '/burst_exponent-overlap-histograms-' + str(sampled_distance) + "m.pdf")
     plt.clf()            
 
 #¢reates and histogram, a boxplot and a density graph for each one of the parameters configuration in every simulation type
@@ -62,8 +66,11 @@ def createSpecificGraphs(simulation, df):
         parameters = parameter_config.replace(simulation + '/', '')
         parameters = parameters.split('-')
         id = str(parameters[0]) +  '-' + str(parameters[1]) + '-' + str(parameters[2]) + '-' + str(parameters[3]) + '-' + str(parameters[4])
-        path = simulation + '/' + id
+        path = simulation + '/' + id + '/graphs/'
         print(path)        
+        if not os.path.isdir(path):
+            os.makedirs(path)
+            
         df = pd.read_csv('./meassurements.csv')
         df = df.loc[(df.meassurement_type == simulation.replace('./', '')) & (df.min_delta_ftm == int(parameters[0])) & (df.burst_period == int(parameters[1])) & (df.burst_exponent == int(parameters[2])) & (df.burst_duration == int(parameters[3])) & (df.ftm_per_burst == int(parameters[4]))]
         if len(df.index) != 0:
