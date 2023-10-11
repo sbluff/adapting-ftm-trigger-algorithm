11106a44f8003cd2548a1a83455bad08253e2f13
sbluff <alonsofmira@gmail.com>
2023-10-11 14:18:04 +0200

    upgrade simulation scheduling

--- ns-allinone-3.33-FTM-SigStr/ns-3.33/scratch/ftm-ranging.cc
+++ ns-allinone-3.33-FTM-SigStr/ns-3.33/scratch/ftm-ranging.cc
@@ -49,6 +49,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <vector>
+#include <cmath>
 
 using namespace ns3;
 
@@ -56,17 +57,20 @@ NS_LOG_COMPONENT_DEFINE ("FtmRanging");
 
 int selected_error_mode = 1; //0: wired, 1: wireless, 2: wireless sig_str, 3: wireless_sig_str with fading
 int session_counter = 0;
-int measurements_per_distance = 10000;
+int measurements_per_distance = 1000;
 std::string file_name;
 int min_delta_ftm, burst_duration, burst_exponent, burst_period, ftm_per_burst;
 double distance;
 Ptr<WirelessFtmErrorModel::FtmMap> map;
 Ptr<WifiNetDevice> _ap;
-Ptr<WifiNetDevice> _sta; 
+Ptr<WifiNetDevice> _sta;
 Address recvAddr;
 std::string mobility_model;
 double t1;
 double t2; //meassure session lenght in time
+bool configuration_finished = false;
+
+void SessionOver (FtmSession session);
 
 
 //configures the parameters of the ftm protocol
@@ -88,21 +92,22 @@ void configureFtm(FtmParams &ftm_params){
 
 //only for circle mean
 void changePosition (Ptr<Node> sta) {
-    if (mobility_model == "circle_mean"){
+    if (mobility_model == "circle_mean" || mobility_model == "test"){
         Ptr<MobilityModel> mobility = sta->GetObject<MobilityModel>();
         double x = cos(2 * M_PI / 360 * (session_counter%360)) * distance;
-        double y = sin(2 * M_PI / 360 * (session_counter%360)) * distance; 
-        
+        double y = sin(2 * M_PI / 360 * (session_counter%360)) * distance;
+
         mobility->SetPosition(Vector(x, y, 0));
     }
 }
+
 
 void changeRadius(Ptr<Node> sta, double radius){
     if (mobility_model == "fix_point"){
         Ptr<MobilityModel> mobility = sta->GetObject<MobilityModel>();
         mobility->SetPosition(Vector(distance, 0, 0));
     }
-    
+
     else if (mobility_model == "circle_mobility"){
         Ptr<CircleMobilityModel> mobility = sta->GetObject<CircleMobilityModel>();
         mobility->SetParameters(Vector (0, 0, 0), double(radius), double(90), double(10));
@@ -111,58 +116,11 @@ void changeRadius(Ptr<Node> sta, double radius){
     std::cout << "Radius changed, new radius " << distance << std::endl;
 }
 
-void SessionOver (FtmSession session)
-{
-  if (distance <= 25){
-    t2 =  Simulator::Now().GetSeconds();; 
-    std::string file_path = "./ftm_ranging/simulations/" + mobility_model + "/" + std::to_string(int(min_delta_ftm)) + '-' + std::to_string(int(burst_duration)) + '-' + std::to_string(int(burst_exponent)) + '-' + std::to_string(int(burst_period)) + '-' + std::to_string(int(ftm_per_burst)) + '/';
-    file_name =  file_path + std::to_string(int(distance)) + 'm';
-    //create folder for files
-    const char* str = file_path.c_str();
-    mkdir(str ,0777);
-
-    std::list<int64_t> rtts = session.GetIndividualRTT();
-    std::list<double> sig_strs = session.GetIndividualSignalStrength();
-
-    std::ofstream output (file_name, std::ofstream::out | std::ofstream::app);
-    int64_t mean_rtt = 0;
-    double mean_sig_str = 0;
-    int count = 0;
-    while (!rtts.empty()){
-        mean_rtt += rtts.front();
-        mean_sig_str += sig_strs.front();
-        sig_strs.pop_front();
-        rtts.pop_front();
-        count++;
-    }
-    if (distance <= 25){
-        mean_rtt = double(mean_rtt / count);
-        mean_sig_str = double(mean_sig_str / count);
-        output << mean_rtt << " " << mean_sig_str << " " << t2 - t1 << "\n";
-    }
-    output.close();
-
-    session_counter++;
-    changePosition(_sta->GetNode());
-    if (session_counter >= measurements_per_distance){
-        session_counter = 0;
-        if (distance <= 25){
-            distance += 5;
-            changeRadius(_sta->GetNode(), double(distance));
-        }
-    }
-  }
-
-  else{
-    std::cout << "Stopping simulator..." << std::endl;
-    Simulator::Stop();
-  }
-}
 
 static void GenerateTraffic ()
 {
   Ptr<RegularWifiMac> _sta_mac = _sta->GetMac()->GetObject<RegularWifiMac>();
-  
+
   Mac48Address to = Mac48Address::ConvertFrom (recvAddr);
 
   Ptr<FtmSession> session = _sta_mac->NewFtmSession(to);
@@ -172,13 +130,7 @@ static void GenerateTraffic ()
     }
 
   Ptr<FtmErrorModel> error_model;
-  if (selected_error_mode == 0) {
-      //create the wired error model
-      Ptr<WiredFtmErrorModel> wired_error = CreateObject<WiredFtmErrorModel> ();
-      wired_error->SetChannelBandwidth(WiredFtmErrorModel::Channel_20_MHz);
-      error_model = wired_error;
-  }
-  else if (selected_error_mode == 1) {
+  if (selected_error_mode == 1) {
       //create wireless error model
       //m_ap has to be created prior
       Ptr<WirelessFtmErrorModel> wireless_error = CreateObject<WirelessFtmErrorModel> ();
@@ -187,16 +139,6 @@ static void GenerateTraffic ()
       wireless_error->SetChannelBandwidth(WiredFtmErrorModel::Channel_20_MHz);
       error_model = wireless_error;
   }
-  else if (selected_error_mode == 2 || selected_error_mode == 3) {
-      //create wireless signal strength error model
-      //m_ap has to be created prior
-      Ptr<WirelessSigStrFtmErrorModel> wireless_sig_str_error = CreateObject<WirelessSigStrFtmErrorModel> ();
-      wireless_sig_str_error->SetFtmMap(map);
-      wireless_sig_str_error->SetNode(_sta->GetNode());
-      wireless_sig_str_error->SetChannelBandwidth(WiredFtmErrorModel::Channel_20_MHz);
-      error_model = wireless_sig_str_error;
-  }
-
 
   //using wired error model in this case
   session->SetFtmErrorModel(error_model);
@@ -208,39 +150,94 @@ static void GenerateTraffic ()
 
   session->SetSessionOverCallback(MakeCallback(&SessionOver));
   session->SessionBegin();
+  t1 = Simulator::Now().GetSeconds();
+
+}
 
 
-  if (int(distance) <= 25){
-    t1 = Simulator::Now().GetSeconds();
-    Simulator::Schedule(Seconds (12), &GenerateTraffic);
+void SessionOver (FtmSession session)
+{
+  if (distance <= 25){
+    t2 =  Simulator::Now().GetSeconds();
+    std::string file_path = "./ftm_ranging/simulations/" + mobility_model + "/" + std::to_string(int(min_delta_ftm)) + '-' + std::to_string(int(burst_duration)) + '-' + std::to_string(int(burst_exponent)) + '-' + std::to_string(int(burst_period)) + '-' + std::to_string(int(ftm_per_burst)) + '/';
+    file_name =  file_path + std::to_string(int(distance)) + 'm';
+    //create folder for files
+    const char* str = file_path.c_str();
+    mkdir(str ,0777);
+
+    std::list<int64_t> rtts = session.GetIndividualRTT();
+    std::list<double> sig_strs = session.GetIndividualSignalStrength();
+
+    std::ofstream output (file_name, std::ofstream::out | std::ofstream::app);
+    int64_t total_rtt = 0;
+    double mean_sig_str = 0;
+    int count = 0;
+    while (!rtts.empty()){
+        total_rtt += rtts.front();
+        mean_sig_str += sig_strs.front();
+        sig_strs.pop_front();
+        rtts.pop_front();
+        count++;
+    }
+    if (distance <= 25){
+        mean_sig_str = double(mean_sig_str / count);
+        output << double(total_rtt / count) << " " << mean_sig_str << " " << t2 - t1 << " " << double(total_rtt/(pow(10,9))) <<  "\n";
+    }
+    output.close();
+
+    session_counter++;
+    changePosition(_sta->GetNode());
+    if (session_counter >= measurements_per_distance){
+        session_counter = 0;
+        if (distance < 25){
+            distance += 5;
+            changeRadius(_sta->GetNode(), double(distance));
+        }
+        else {
+          configuration_finished = true;
+          std::cout << "configuration finished!" << std::endl;
+          std::cout << "Stopping simulator..." << std::endl;
+          Simulator::Stop();
+        }
+    }
+
+    if (int(distance) <= 25 && !configuration_finished){
+      Simulator::Schedule(Seconds (0.1), &GenerateTraffic);
+    }
+
+  }
+
+  else{
+
   }
 }
 
 //loads into configurations the set of parameters that will be used during each one of the mobility_model simulations
 void loadConfigurations(std::vector<std::vector<int>>& configurations){
-//   configurations.push_back({15,10,1,10,2}); 
-  configurations.push_back({15,10,1,10,3}); 
-  configurations.push_back({15,10,1,10,4});   
-  configurations.push_back({15,10,1,10,5});   
-  configurations.push_back({15,10,2,10,2}); 
-  configurations.push_back({15,10,2,10,3}); 
-  configurations.push_back({15,10,2,10,4}); 
-  configurations.push_back({15,10,2,10,5}); 
-  configurations.push_back({15,10,3,10,2}); 
-  configurations.push_back({15,10,3,10,3}); 
-  configurations.push_back({15,10,3,10,4}); 
-  configurations.push_back({15,10,3,10,5}); 
-  configurations.push_back({15,10,4,10,2}); 
-  configurations.push_back({15,10,4,10,3}); 
-  configurations.push_back({15,10,4,10,4}); 
-  configurations.push_back({15,10,4,10,5}); 
+  configurations.push_back({15,10,1,10,2});
+  configurations.push_back({15,10,1,10,3});
+  configurations.push_back({15,10,1,10,4});
+  configurations.push_back({15,10,1,10,5});
+  configurations.push_back({15,10,2,10,2});
+  configurations.push_back({15,10,2,10,3});
+  configurations.push_back({15,10,2,10,4});
+  configurations.push_back({15,10,2,10,5});  
+  configurations.push_back({15,10,3,10,2});
+  configurations.push_back({15,10,3,10,3});
+  configurations.push_back({15,10,3,10,4});
+  configurations.push_back({15,10,3,10,5});
+  configurations.push_back({15,10,4,10,2});
+  configurations.push_back({15,10,4,10,3});
+  configurations.push_back({15,10,4,10,4});
+  configurations.push_back({15,10,4,10,5});
 }
 
 //loads in models the type of simulations that will take place
 void loadModels(std::vector<std::string>& models){
-//   models.push_back("fix_position"); 
-  models.push_back("circle_mean"); 
-//   models.push_back("circle_velocity");   
+  // models.push_back("test");
+  // models.push_back("circle_mean");
+  // models.push_back("circle_velocity");
+  models.push_back("fix_position");
 }
 
 //based on mobility_mode variable this function assigns the container different mobility modes and sets its parameters
@@ -259,7 +256,7 @@ void assignMobilityModel(NodeContainer& initiatingNodes, NodeContainer& receivin
         m1->DoSetPosition(Vector (0, 0, 0));
     }
 
-    else if (mobility_model == "circle_mean" || mobility_model == "fix_position"){
+    else if (mobility_model == "circle_mean" || mobility_model == "test"){
         MobilityHelper constantPositionMobility;
         constantPositionMobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
         constantPositionMobility.Install (initiatingNodes);
@@ -267,7 +264,18 @@ void assignMobilityModel(NodeContainer& initiatingNodes, NodeContainer& receivin
         Ptr<ConstantPositionMobilityModel> m1 = initiatingNodes.Get(0)->GetObject<ConstantPositionMobilityModel>();
         Ptr<ConstantPositionMobilityModel> m2 = receivingNodes.Get(0)->GetObject<ConstantPositionMobilityModel>();
         m1->DoSetPosition(Vector (0, 0, 0));
-        m2->DoSetPosition(Vector (distance, 0, 0));
+        m2->DoSetPosition(Vector (0, 0, 0));
+    }
+
+    else if (mobility_model == "fix_position"){
+              MobilityHelper constantPositionMobility;
+        constantPositionMobility.SetMobilityModel ("ns3::ConstantPositionMobilityModel");
+        constantPositionMobility.Install (initiatingNodes);
+        constantPositionMobility.Install (receivingNodes);
+        Ptr<ConstantPositionMobilityModel> m1 = initiatingNodes.Get(0)->GetObject<ConstantPositionMobilityModel>();
+        Ptr<ConstantPositionMobilityModel> m2 = receivingNodes.Get(0)->GetObject<ConstantPositionMobilityModel>();
+        m1->DoSetPosition(Vector (distance, 0, 0));
+        m2->DoSetPosition(Vector (0, 0, 0));
     }
 
     else{
@@ -280,6 +288,7 @@ void assignMobilityModel(NodeContainer& initiatingNodes, NodeContainer& receivin
 // then runs simulator
 void runSimulation(const std::vector<int>& configuration){
     ///////// PARAMETERS
+    configuration_finished = false;
     std::cout << "New config" << std::endl;
     min_delta_ftm = configuration[0];
     burst_duration = configuration[1];
@@ -301,9 +310,10 @@ void runSimulation(const std::vector<int>& configuration){
     WifiHelper wifi;
     wifi.SetStandard (WIFI_STANDARD_80211n_2_4GHZ);
 
-    YansWifiPhyHelper wifiPhy;
+
     // This is one parameter that matters when using FixedRssLossModel
     // set it to zero; otherwise, gain will be added
+    YansWifiPhyHelper wifiPhy;
     wifiPhy.Set ("RxGain", DoubleValue (0) );
     wifiPhy.Set ("TxPowerStart", DoubleValue(14));
     wifiPhy.Set ("TxPowerEnd", DoubleValue(14));
@@ -317,21 +327,14 @@ void runSimulation(const std::vector<int>& configuration){
 
 //  wifiChannel.AddPropagationLoss ("ns3::ThreeGppIndoorOfficePropagationLossModel");
 
-    if (selected_error_mode == 0 || selected_error_mode == 1) {
+    if (selected_error_mode == 1) {
         // The below FixedRssLossModel will cause the rss to be fixed regardless
         // of the distance between the two stations, and the transmit power
         wifiChannel.AddPropagationLoss ("ns3::FixedRssLossModel","Rss",DoubleValue (-40));
     }
-    else if (selected_error_mode == 2) {
-        wifiChannel.AddPropagationLoss ("ns3::ThreeLogDistancePropagationLossModel");
-    }
-    else if (selected_error_mode == 3) {
-        wifiChannel.AddPropagationLoss ("ns3::ThreeLogDistancePropagationLossModel");
-        wifiChannel.AddPropagationLoss ("ns3::NakagamiPropagationLossModel");
-    }
-    wifiPhy.SetChannel (wifiChannel.Create ());
 
-    // Add a mac and disable rate control
+
+    wifiPhy.SetChannel (wifiChannel.Create ());
     WifiMacHelper wifiMac;
     wifi.SetRemoteStationManager ("ns3::ConstantRateWifiManager");
 
@@ -341,12 +344,13 @@ void runSimulation(const std::vector<int>& configuration){
     wifiMac.SetType ("ns3::StaWifiMac",
                     "Ssid", SsidValue (ssid));
     //  wifiMac.SetType ("ns3::StaWifiMac");
-    NetDeviceContainer staDevice = wifi.Install (wifiPhy, wifiMac, initiatingNodesContainer.Get (0));
+    NetDeviceContainer staDevice = wifi.Install (wifiPhy, wifiMac, initiatingNodesContainer.Get(0));
     NetDeviceContainer devices = staDevice;
+
     // setup ap.
     wifiMac.SetType ("ns3::ApWifiMac",
                     "Ssid", SsidValue (ssid));
-    NetDeviceContainer apDevice = wifi.Install (wifiPhy, wifiMac, receivingNodesContainer.Get (0));
+    NetDeviceContainer apDevice = wifi.Install (wifiPhy, wifiMac, receivingNodesContainer.Get(0));
     devices.Add (apDevice);
 
 
@@ -363,7 +367,7 @@ void runSimulation(const std::vector<int>& configuration){
     wifiPhy.EnablePcap ("ftm-ranging", devices);
     Simulator::ScheduleNow (&GenerateTraffic);
 
-    Simulator::Stop (Seconds (300000.0));
+    Simulator::Stop (Seconds (50000.0));
     Simulator::Run ();
     Simulator::Destroy ();
 }
@@ -374,14 +378,14 @@ int main (int argc, char *argv[])
   selected_error_mode = 1;
   map = CreateObject<WirelessFtmErrorModel::FtmMap> ();
   if (selected_error_mode != 0) {
-      map->LoadMap ("src/wifi/ftm_map/FTM_Wireless_Error_200.map");
+      map->LoadMap ("src/wifi/ftm_map/FTM_Wireless_Error.map");
   }
   std::cout << "Multipath Map loaded!" << std::endl;
   //set time resolution to pico seconds for the time stamps, as default is in nano seconds. IMPORTANT
   Time::SetResolution(Time::PS);
 
   std::vector<std::vector<int>> configurations;
-  loadConfigurations(configurations); 
+  loadConfigurations(configurations);
 
   std::vector<std::string> models;
   loadModels(models);
@@ -390,6 +394,6 @@ int main (int argc, char *argv[])
         distance = 10;
         mobility_model = models[i];
         runSimulation(configurations[j]);
-    } 
+    }
   return 0;
 }
