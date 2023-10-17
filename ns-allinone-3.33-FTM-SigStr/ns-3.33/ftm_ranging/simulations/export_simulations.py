3e61255c91cbfbf1ac9020ad91657094c5ff8e70
sbluff <alonsofmira@gmail.com>
2023-10-18 01:09:58 +0200

    add eficiency field

--- ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/export_simulations.py
+++ ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/export_simulations.py
@@ -16,7 +16,7 @@ def file_is_empty(filename):
     return True
 
 with open('./data/data.csv', 'w', newline='') as csvfile:
-    fieldnames = ['real_distance','meassured_distance','meassurement_type','min_delta_ftm','burst_period','burst_exponent','burst_duration','ftm_per_burst', 'error', 'session_time', 'channel_time', 'channel_usage']
+    fieldnames = ['real_distance','meassured_distance','meassurement_type','min_delta_ftm','burst_period','burst_exponent','burst_duration','ftm_per_burst', 'error', 'session_time', 'channel_time', 'channel_usage', 'efficiency']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     subfolders = [ f.path for f in os.scandir('./') if f.is_dir() ]
@@ -34,9 +34,20 @@ with open('./data/data.csv', 'w', newline='') as csvfile:
                     curr_measurement = np.loadtxt(file)
                     print(curr_measurement)
                     for m in curr_measurement:
-                        value = int(m[0]) / 2 / (10000 * 0.3)
-                        channel_usage = float(m[3]/m[2])*100
-                        writer.writerow({'real_distance': curr_distance, 'meassured_distance': value, 'meassurement_type': meassurment_type, 'min_delta_ftm': parameters[0],'burst_period': parameters[1],'burst_exponent': parameters[2],'burst_duration': parameters[3],'ftm_per_burst':parameters[4], 'error': value-curr_distance, 'session_time': m[2], 'channel_time': m[3], 'channel_usage': channel_usage})
+                        value = int(m[1]) / 2 / (10000 * 0.3)
+                        channel_usage = float(m[4]/m[3])*100
+                        writer.writerow({
+                            'real_distance': m[0],
+                            'meassured_distance': value,
+                            'meassurement_type': meassurment_type,
+                            'min_delta_ftm': parameters[0],'burst_period': parameters[1],
+                            'burst_exponent': parameters[2],'burst_duration': parameters[3],
+                            'ftm_per_burst':parameters[4],
+                            'error': value-curr_distance,
+                            'session_time': m[3], 'channel_time': m[4],
+                            'channel_usage': channel_usage,
+                            'efficiency':1/(float(channel_usage * abs(value-m[0]))) if value-m[0] else 0
+                        })
 
 print("Data exported correctly!")
             
