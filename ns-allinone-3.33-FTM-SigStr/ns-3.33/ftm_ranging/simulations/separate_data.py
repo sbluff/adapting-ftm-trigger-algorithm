27089164580bbaa95e818b67b4267073bc37750a
sbluff <alonsofmira@gmail.com>
2023-10-21 13:14:15 +0200

    dont delete data.csv

--- ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/separate_data.py
+++ ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/separate_data.py
@@ -6,13 +6,12 @@ def separate_csv():
     simulation_types = ['fix_position', 'circle_mean', 'circle_velocity', 'brownian']
     df = pd.read_csv('data/data.csv')
     print(df.size)
-    print(df.head)
     for simulation in simulation_types:
         aux = df.loc[(df.simulation_type == simulation)]
         print(aux.size)
         aux.to_csv('data/data-' + simulation + '.csv')
         
-    os.remove("data/data.csv")  
+    # os.remove("data/data.csv")  
     
 
 separate_csv()      
