172b4b2b70929654bf7b9c439d729b25a640a250
sbluff <alonsofmira@gmail.com>
2023-10-29 17:31:49 +0100

    format axis titles for static graphs

--- ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/analysis/generate_simulation_type_graphs.py
+++ ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/analysis/generate_simulation_type_graphs.py
@@ -55,7 +55,7 @@ def velocityViolinPlots(df, simulation_type):
                 
                 if hist.size != 0:
                     sns.set(style="whitegrid")    
-                    sns.violinplot(data = hist, y = field, hue = 'velocity', ylim=(-2*df[field].mean(), 2*df[field].mean()), x = parameter, grid=True, density_norm="area", common_norm="true")               
+                    sns.violinplot(data = hist, y = field, hue = 'velocity', ylim=(-2*hist[field].mean(), 2*hist[field].mean()), x = parameter, grid=True, density_norm="area", common_norm="true")               
                         
                     plt.xlabel(parameter.replace("_", " "))
                     plt.ylabel(field.replace("_", " ") + " (" + result_fields_units[counter] + ")")    
@@ -74,12 +74,15 @@ def velocityViolinPlots(df, simulation_type):
 def staticViolinPlots(df, simulation_type):
     path = './simulation_type/' + simulation_type + '/'     
     parameters = ['ftm_per_burst', 'burst_duration', 'burst_period', 'burst_exponent']
-    result_fields = ['error', 'channel_time', 'channel_usage', 'session_time', 'efficiency']
+    
+    result_fields = ['efficiency', 'error', 'channel_time', 'channel_usage', 'session_time']
+    result_fields_units = ['1/(m*s)', 'm', 's', '%', 's']
     
     for parameter in parameters:
         iterable_parameters = copy.deepcopy(parameters)
         iterable_parameters.remove(parameter)
         all_combinations = generateDataStructure(iterable_parameters, df)  
+        counter = 0
         for field in result_fields:
             file_path = path + parameter + '-' + field + '/'
             
@@ -89,7 +92,12 @@ def staticViolinPlots(df, simulation_type):
             for combination in all_combinations:
                 # file_path = path + dumps(combination) + '.pdf'
                 pdf_name = file_path + str(combination).replace(" ", "")+".pdf" 
-                hist = df
+                hist = copy.deepcopy(df)
+                y = hist[field]
+                removed_outliers = y.between(y.quantile(.05), y.quantile(.9))
+                index_names = hist[~removed_outliers].index
+                hist.drop(index_names, inplace=True)
+                           
                 # print(combination)
                 for x in combination:
                     hist = hist[hist[x] == combination[x]]
@@ -97,13 +105,14 @@ def staticViolinPlots(df, simulation_type):
                 
                 if hist.size != 0:
                     values = df[parameter].unique()
-                    sns.violinplot(data = hist, x = parameter, density_norm="area", common_norm="true", cut=0, grid=True, y = field)
-                    plt.xlabel(parameter)
-                    plt.ylabel(field)    
+                    sns.violinplot(data = hist, x = parameter, ylim=(-2*hist[field].mean(), 2*hist[field].mean()), density_norm="area", common_norm="true", cut=0, grid=True, y = field)
+                    plt.xlabel(parameter.replace("_", " "))
+                    plt.ylabel(field.replace("_", " ") + "(" + result_fields_units[counter] + ")")    
                     plt.savefig(pdf_name)
                     print(pdf_name)
                     # plt.show()
                     plt.clf()
+        counter += 1            
 
 
 circle_mean_data = pd.read_csv('../data/data-circle_mean.csv')    
@@ -112,8 +121,8 @@ fix_position_data = pd.read_csv('../data/data-fix_position.csv')
 brownian_data = pd.read_csv('../data/data-brownian.csv')   
 all_data = pd.concat([circle_mean_data, circle_velocity_data, fix_position_data, brownian_data]) #merge all data in a data frame
 
-velocityViolinPlots(brownian_data, 'brownian')
-velocityViolinPlots(circle_velocity_data, 'circle_velocity')
-# staticViolinPlots(fix_position_data, 'fix_position')
-# staticViolinPlots(circle_mean_data, 'circle_mean')
+# velocityViolinPlots(brownian_data, 'brownian')
+# velocityViolinPlots(circle_velocity_data, 'circle_velocity')
+staticViolinPlots(fix_position_data, 'fix_position')
+staticViolinPlots(circle_mean_data, 'circle_mean')
 print("Done...")
