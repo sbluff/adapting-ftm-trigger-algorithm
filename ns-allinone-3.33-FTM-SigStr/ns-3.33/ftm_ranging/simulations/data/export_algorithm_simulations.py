a62385b3264e5fc7f351d49a82fcd6cb5ee61dc6
sbluff <alonsofmira@gmail.com>
2023-12-05 14:38:13 +0100

    refactor

--- ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/data/export_algorithm_simulations.py
+++ ns-allinone-3.33-FTM-SigStr/ns-3.33/ftm_ranging/simulations/data/export_algorithm_simulations.py
@@ -33,7 +33,7 @@ def file_is_empty(filename):
 files = ['./adaptive-algorithm/adaptive-algorithm']
 
 with open('./data-algorithm.csv', 'w+', newline='') as csvfile:
-    fieldnames = ['real_distance','meassured_distance','min_delta_ftm','burst_period','burst_exponent','burst_duration','ftm_per_burst', 'error', 'session_time', 'channel_time', 'channel_usage', 'efficiency', 'velocity', 'x_position', 'y_position', 'version', 'ts', 'pause', 'speed']
+    fieldnames = ['real_distance','meassured_distance','error', 'error_area','min_delta_ftm','burst_period','burst_exponent','burst_duration','ftm_per_burst','session_time', 'channel_time', 'channel_usage', 'efficiency', 'velocity', 'x_position', 'y_position', 'version', 'ts', 'pause', 'speed']
     writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
     writer.writeheader()
     count = 0
@@ -41,49 +41,43 @@ with open('./data-algorithm.csv', 'w+', newline='') as csvfile:
         if not file_is_empty(file):
             curr_measurement = np.loadtxt(file)
             ts = 0
-            
-            t0 = float(previous_measurement['ts'])
-            t1 = float(m['ts'])
-            r0 = previous_measurement['real_distance']
-            r1 = m['real_distance']
-            m = previous_measurement['meassured_distance']
 
-            previous_measurement = []
-            for m in curr_measurement:
+            previous_measurement = curr_measurement[0]
+            for measurement in curr_measurement:
                 error_area = 0
-                if previous_measurement != []:
-                    t0 = float(previous_measurement['ts'])
-                    t1 = float(m['ts'])
-                    r0 = previous_measurement['real_distance']
-                    r1 = m['real_distance']
-                    m = previous_measurement['meassured_distance']
-                    error_area = quad(f1, t0, t1)[0] * ((r1-r0)/(t1-t0)) + quad(f2, t0, t1)[0] * (t1*r0 - t0*r1)/(t1-t0) - quad(g, t0, t1)[0] * m
-                value = int(m[6]) / 2 / (10000 * 0.3)
-                channel_usage = abs(float(m[9]/m[8])*100)
+                t0 = ts
+                t1 = ts + abs(measurement[8])
+                print(previous_measurement)
+                r0 = previous_measurement[0]
+                r1 = measurement[0]
+                m = previous_measurement[1]
+                error_area = quad(f1, t0, t1)[0] * ((r1-r0)/(t1-t0)) + quad(f2, t0, t1)[0] * (t1*r0 - t0*r1)/(t1-t0) - quad(g, t0, t1)[0] * m
+                value = int(measurement[6]) / 2 / (10000 * 0.3)
+                channel_usage = abs(float(measurement[9]/measurement[8])*100)
                 writer.writerow({
-                    'real_distance': m[5],
+                    'real_distance': measurement[5],
                     'meassured_distance': value,
-                    'min_delta_ftm': m[0],
-                    'burst_period': m[1],
-                    'burst_exponent': m[2],
-                    'burst_duration': m[3],
-                    'ftm_per_burst':m[4],
-                    'error': value-m[5],
+                    'error': value-measurement[5],
                     'error_area': error_area,
-                    'session_time': abs(m[8]), 
-                    'channel_time': abs(m[9]),
+                    'min_delta_ftm': measurement[0],
+                    'burst_period': measurement[1],
+                    'burst_exponent': measurement[2],
+                    'burst_duration': measurement[3],
+                    'ftm_per_burst':measurement[4],
+                    'session_time': abs(measurement[8]), 
+                    'channel_time': abs(measurement[9]),
                     'channel_usage': channel_usage,
-                    'efficiency':1/(float(channel_usage * abs(value-m[5]))) if channel_usage * abs(value-m[5]) != 0 else 0,
-                    'velocity': m[10],
-                    'x_position': m[11],
-                    'y_position': m[12],
-                    'version': m[14] if "static-algorithm" not in file else 0,
+                    'efficiency':1/(float(channel_usage * abs(error_area))) if channel_usage * abs(error_area) != 0 else 0,
+                    'velocity': measurement[10],
+                    'x_position': measurement[11],
+                    'y_position': measurement[12],
+                    'version': measurement[14] if "static-algorithm" not in file else 0,
                     'ts': ts,
-                    'pause': m[15],
-                    'speed': m[16],
+                    'pause': measurement[15],
+                    'speed': measurement[16],
                 })
-                ts += abs(m[8])
-                previous_measurement = m
+                ts = t1
+                previous_measurement = measurement
         count += 1       
 
 print("Data exported correctly!")
